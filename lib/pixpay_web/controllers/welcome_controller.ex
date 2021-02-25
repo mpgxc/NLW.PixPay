a9b2c8f4480fe97e3c459b4adea35292be1b28f4
mpgxc <mpgx5.c@gmail.com>
2021-02-25 00:48:53 -0300

    feat: Retornando módulo de soma no controller do app

--- lib/pixpay_web/controllers/welcome_controller.ex
+++ lib/pixpay_web/controllers/welcome_controller.ex
@@ -1,7 +1,23 @@
 defmodule PixpayWeb.WelcomeController do
   use PixpayWeb, :controller
 
-  def index(conn, _params) do
-    text(conn, "Bem Vindo a minha primeira API com Elixir/Phoenix")
+  alias Pixpay.Numbers
+
+  def index(conn, %{"filename" => filename}) do
+    filename
+    |> Numbers.sum_from_file()
+    |> handle_response(conn)
+  end
+
+  defp handle_response({:ok, %{result: result}}, conn) do
+    conn
+    |> put_status(:ok)
+    |> json(%{message: "Welcome to PixPay API! Result: #{result}"})
+  end
+
+  defp handle_response({:error, reason}, conn) do
+    conn
+    |> put_status(:bad_request)
+    |> json(reason)
   end
 end
